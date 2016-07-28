0bb9227688e3f1eec6a03a7f45a2edacadd4fd81
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-07-28 15:57:36 +0200

    Refactor vgg16 test

--- test_vgg16.py
+++ test_vgg16.py
@@ -3,25 +3,32 @@ import tensorflow as tf
 
 import vgg16
 import utils
+import time
 
-img1 = utils.load_image("./test_data/tiger.jpeg")
-img2 = utils.load_image("./test_data/puzzle.jpeg")
+start = time.time()
+def tick():
+    print(time.time() - start)
 
-batch1 = img1.reshape((1, 224, 224, 3))
-batch2 = img2.reshape((1, 224, 224, 3))
+filenames = ["./test_data/tiger.jpeg", "./test_data/puzzle.jpeg", "./test_data/tiger.jpeg", "./test_data/puzzle.jpeg"]
+images = [utils.load_image(f) for f in filenames]
+batches = [im.reshape((1, 224, 224, 3)) for im in images]
 
-batch = np.concatenate((batch1, batch2), 0)
+batch = np.concatenate(batches, 0)
+
+tick()
 
 with tf.Session(
         config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.7)))) as sess:
-    images = tf.placeholder("float", [2, 224, 224, 3])
+    images = tf.placeholder("float", [len(filenames), 224, 224, 3])
     feed_dict = {images: batch}
 
+    tick()
     vgg = vgg16.Vgg16()
     with tf.name_scope("content_vgg"):
         vgg.build(images)
+    tick()
+    probs = sess.run(vgg.prob, feed_dict=feed_dict)
+    tick()
+    for pr in probs:
+        utils.print_prob(pr, './synset.txt')
 
-    prob = sess.run(vgg.prob, feed_dict=feed_dict)
-    print(prob)
-    utils.print_prob(prob[0], './synset.txt')
-    utils.print_prob(prob[1], './synset.txt')
